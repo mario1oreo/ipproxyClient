d6d004e084f1765dc8d37f9eeaae44b5d22792e8
mario1oreo <mario_oreo1@163.com>
2018-01-24 00:12:22 +0800

    修改redis方式的动态预热，从指定的预热队列中获取10个url为测试连接

--- utils/proxy/OfflineDynamicProxy.java
+++ utils/proxy/OfflineDynamicProxy.java
@@ -4,15 +4,16 @@ package com.fosun.financial.data.proxy.ipproxyclient.utils.proxy;
 import com.virjar.dungproxy.client.ippool.PreHeater;
 import com.virjar.dungproxy.client.ippool.config.DungProxyContext;
 import com.virjar.dungproxy.client.ippool.strategy.impl.DefaultContentProxyChecker;
+import com.virjar.dungproxy.client.util.RedisTools;
 import lombok.Data;
+
+import java.util.List;
 
 /**
  * 启动离线预热--动态
  *
  * @author mario1oreo
  * @date 2018-1-16 15:33:47
- *
- *
  */
 @Data
 public class OfflineDynamicProxy implements Runnable {
@@ -28,16 +29,27 @@ public class OfflineDynamicProxy implements Runnable {
         this.type = type;
         this.topicName = topicName;
         this.expectContent = expectContent;
+
     }
 
     @Override
     public void run() {
         DungProxyContext dungProxyContext = DungProxyContext.create();
         //TODO 创建动态切换redis  kafka  等消息中间件的Checker   从不同的消息队列中获取动态变动的预热IP
+
+
         dungProxyContext.setDefaultCoreSize(100).genDomainContext(getDomain()).setProxyChecker(new DefaultContentProxyChecker(getExpectContent(), getUserID(), getDomain()));
 
         PreHeater preHeater = dungProxyContext.getPreHeater();
-        preHeater.addTask("");
+        RedisTools redisTools = new RedisTools();
+        //redis
+        if ("1".equals(getType())) {
+            List<String> tasks = redisTools.lrange(topicName, 0, 10);
+            tasks.forEach(task -> preHeater.addTask(task));
+            //kafka
+        } else if ("2".equals(getType())) {
+            preHeater.addTask("");
+        }
         preHeater.setThreadNumber(100);
         preHeater.doPreHeat();
         preHeater.destroy();
